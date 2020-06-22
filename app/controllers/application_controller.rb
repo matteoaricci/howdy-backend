5cdd8e67f8f98cade443f8a38667397821ce0509
matteoaricci <matteo.a.ricci@gmail.com>
2020-06-22 14:23:18 -0400

    login and new user work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::API
-    respond_to :html, :json
+    respond_to :json
 
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :authenticate_user!
+
+    protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-     end
+        devise_parameter_sanitizer.permit(:login, keys: [:username, :email, :password])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    end
 
 end
